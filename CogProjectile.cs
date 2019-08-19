8f0491b98d8b5c2a51e996119e09ac1c3a82266f
Ryan Remmen <33638558+RyanRemmen@users.noreply.github.com>
2019-08-19 14:18:45 -0700

    Add files via upload

---
+++ CogProjectile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CogProjectile : MonoBehaviour
+{
+
+    public AudioClip throwCog;
+    public ParticleSystem projectileCollisionPrefab;
+
+    private Rigidbody2D rigidbody2d;
+
+    private float timer;
+    private float resetTimer = 3.0f;
+
+    void Awake()
+    {
+        this.rigidbody2d = GetComponent<Rigidbody2D>();
+        this.timer = resetTimer;
+    }
+
+    private void Update()
+    {
+        this.timer -= Time.deltaTime;
+
+        if (this.timer <= 0)
+        {
+            Destroy(gameObject);
+            this.timer = this.resetTimer;
+        }
+    }
+
+    public void Launch( Vector2 direction, float force, AudioSource audiosource)
+    {
+        RubyController controller = GetComponent<RubyController>();
+
+        controller = audiosource.GetComponent<RubyController>();
+        controller.PlaySound(throwCog);
+
+        this.rigidbody2d.AddForce(direction * force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ParticleSystem cogCollisionEffect =
+            Instantiate(
+                this.projectileCollisionPrefab,
+                this.rigidbody2d.position,
+                Quaternion.identity);
+
+        EnemyRobotController enemy = other.collider.GetComponent<EnemyRobotController>();
+
+        if (enemy != null)
+        {
+            enemy.Fix();
+        }
+        Destroy(gameObject);
+        this.timer = this.resetTimer;
+    }
+}
---
+++ EnemyRobotController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography;
+
+public class EnemyRobotController : MonoBehaviour
+{
+    public bool vertical = false;
+    public float moveSpeed = 3.0f;
+    private float direction = 1.0f;
+
+    public int maxHealth = 5;
+    public int currHealth;
+    public int health { get { return this.currHealth; } }
+    private bool broken;
+
+    public float changeTime = 3.0f;
+    private float timer;
+
+    public ParticleSystem smokeEffect;
+
+    public AudioClip sfxRobotFixed;
+    public AudioClip sfxRobotBroken;
+    public AudioClip sfxEnemyHit1, sfxEnemyHit2;
+
+    private Rigidbody2D rigidbody2d;
+    private Vector2 position;
+    private Animator animator;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        this.rigidbody2d = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+        this.audioSource = GetComponent<AudioSource>();
+
+        this.timer = this.changeTime;
+        this.currHealth = this.maxHealth;
+        this.broken = true;
+    }
+
+    void Update()
+    {
+
+        if (!this.broken)
+        {
+            return;
+        }
+        else
+        {
+            this.timer -= Time.deltaTime;
+        }
+
+        if (this.timer <= 0)
+        {
+            this.direction = this.direction * -1;
+            this.timer = this.changeTime;
+        }
+
+        this.position = this.rigidbody2d.position;
+
+        if (this.vertical)
+        {
+            this.animator.SetFloat("MoveX", 0.0f);
+            this.animator.SetFloat("MoveY", this.direction);
+            this.position.y += Time.deltaTime * this.moveSpeed * this.direction;
+        }
+        else
+        {
+            this.animator.SetFloat("MoveX", this.direction);
+            this.animator.SetFloat("MoveY", 0.0f);
+            this.position.x += Time.deltaTime * this.moveSpeed * this.direction;
+        }
+        this.rigidbody2d.MovePosition(this.position);
+    }
+
+    public void Fix()
+    {
+        this.playSound(this.sfxEnemyHit1);
+        this.playSound(this.sfxRobotFixed);
+        this.audioSource.clip = null;
+        this.broken = false;
+        this.animator.SetTrigger("Fixed");
+        this.rigidbody2d.simulated = false;
+        this.smokeEffect.Stop();
+    }
+
+    public void playSound(AudioClip clip)
+    {
+        this.audioSource.PlayOneShot(clip);
+    }
+
+    
+}
